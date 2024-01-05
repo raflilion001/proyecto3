ddc50f481f5f5bf593d09b89a05a6f01dd243444
Rafael Ivan Acevedo <rafael.ivan.acevedo@gmail.com>
2024-01-05 14:11:14 -0300

    cambios de 5/1/2024

--- preentrega3/core/views.py
+++ preentrega3/core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 #crea tus vistas
 
 def index(request):
-    return render(request,"core/index.html") 
+    return render(request,"core/index.html",{"nombre":"ferreteria"}) 
