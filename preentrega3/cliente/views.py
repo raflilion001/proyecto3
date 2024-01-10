0ac3dddbd37529b148256ed1f2373e25fc5d39f3
Rafael Ivan Acevedo <rafael.ivan.acevedo@gmail.com>
2024-01-10 00:31:42 -0300

    actualizacion 10/1/2024

--- preentrega3/cliente/views.py
+++ preentrega3/cliente/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from .models import Cliente
+
 # Create your views here.
 
 
+def index(request):
+    clientes = Cliente.objects.all()
+    return render(request, "cliente/index.html",{"clientes":clientes})
+
+# primer argumento un request,segundo el
+#nombre de la carpeta contenedora /y el template, el contexto en forma de diccionario con un valor de instancia 
+#que en este caso es la variable  clientes
+
+#de esta forma le pasamos al html todo el contenido de la base de datos de este modelo   
+#
--- preentrega3/config/urls.py
+++ preentrega3/config/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include,path
 from .views import saludo,saludo2,nombre,fecha_hora,tirar_dado
@@ -28,7 +13,7 @@ urlpatterns = [
     path ('',include("core.urls")),
     #en include se pone el nombre de la applicacion y el nombre del modulo 
     
-    
-    
-     
+    path ("",include("cliente.urls")),#nombre de la aplicaion (cliente) y el modulo que es urls
+        
+      
 ]
